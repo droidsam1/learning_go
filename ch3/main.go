3e336aff8cb10187a66cbfe1e12537a9c4b33507
droidsam1 <droidsam@gmail.com>
2025-11-02 10:37:59 +0100

    solution to exercise 1

--- ch3/main.go
+++ ch3/main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello Go")
+	greetings := []string{"Hello", "Hola", "नमस्कार", "こんにちは", "Привіт"}
+	fmt.Println(greetings[:2])  //first two elements
+	fmt.Println(greetings[1:4]) //second, third and fourth variables
+	fmt.Println(greetings[3:5]) //fourth and fifth variables
 }
